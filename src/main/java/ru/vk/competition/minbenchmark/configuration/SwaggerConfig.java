568253a521a182b979d63be75af7a97705fcd779
PavelSoldatov <pavsoldatov96@gmail.com>
2022-08-12 15:37:51 +0300

    Working var2

--- src/main/java/ru/vk/competition/minbenchmark/configuration/SwaggerConfig.java
+++ src/main/java/ru/vk/competition/minbenchmark/configuration/SwaggerConfig.java
@@ -1,5 +1,5 @@
 package ru.vk.competition.minbenchmark.configuration;
-
+//
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import springfox.documentation.builders.PathSelectors;
@@ -7,7 +7,7 @@ package ru.vk.competition.minbenchmark.configuration;
 //import springfox.documentation.spi.DocumentationType;
 //import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//
 //@Configuration
 //@EnableSwagger2
 //public class SwaggerConfig {
@@ -15,8 +15,9 @@ package ru.vk.competition.minbenchmark.configuration;
 //    public Docket api() {
 //        return new Docket(DocumentationType.SWAGGER_2)
 //                .select()
-//                .apis(RequestHandlerSelectors.basePackage("ru.vk.competition.minbenchmark"))
+//                .apis(RequestHandlerSelectors.any())
 //                .paths(PathSelectors.any())
-//                .build();
+//                .build()
+//                .pathMapping("/");
 //    }
 //}
--- src/main/java/ru/vk/competition/minbenchmark/service/TableService.java
+++ src/main/java/ru/vk/competition/minbenchmark/service/TableService.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Data
 @Service
@@ -12,12 +13,10 @@ public class TableService {
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Transactional
     public void createTable() {
-        jdbcTemplate.execute("CREATE TABLE TBL_EMPLOYEES (\n" +
-                "  id INT AUTO_INCREMENT  PRIMARY KEY,\n" +
-                "  first_name VARCHAR(250) NOT NULL,\n" +
-                "  last_name VARCHAR(250) NOT NULL,\n" +
-                "  email VARCHAR(250) DEFAULT NULL\n" +
-                ");");
+        String sql = "CREATE TABLE zalupa (id integer);";
+        jdbcTemplate.execute(sql);
+        int a = 5;
     }
 }
